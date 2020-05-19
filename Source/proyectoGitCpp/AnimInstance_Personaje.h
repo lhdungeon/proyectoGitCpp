21fa4cc7dd7fba5e2d0ed05dfa2dc037f12d3ee9
lhdungeon <64814901+lhdungeon@users.noreply.github.com>
2020-05-19 17:27:17 -0300

    Recuperacion

---
+++ Source/proyectoGitCpp/AnimInstance_Personaje.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "AnimInstance_Personaje.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROYECTOGITCPP_API UAnimInstance_Personaje : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
